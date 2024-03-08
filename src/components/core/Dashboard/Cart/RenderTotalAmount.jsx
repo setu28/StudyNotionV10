34f5062e4a8cbb29455bc0397363f4363f512346
Setu Narayan <setu.shubham28@gmail.com>
2024-03-08 19:26:00 +0530

    Seven commit

--- src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
+++ src/components/core/Dashboard/Cart/RenderTotalAmount.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux"
+import { useNavigate } from "react-router-dom"
+
 import IconBtn from '../../../common/IconBtn';
+import { BuyCourse } from '../../../../services/operations/studentFeaturesAPI';
 
 const RenderTotalAmount = () => {
 
-    const {total, cart} = useSelector((state) => state.cart);
+  const { total, cart } = useSelector((state) => state.cart)
+  const { token } = useSelector((state) => state.auth)
+  const { user } = useSelector((state) => state.profile)
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
 
     const handleBuyCourse = () => {
         const courses = cart.map((course) => course._id);
         console.log("Bought these course:", courses);
+        BuyCourse(token, courses, user, navigate, dispatch)
         //TODO: API integrate -> payment gateway tak leke jaegi
     }
   return (
